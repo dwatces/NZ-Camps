672e1cb0d80fff7da6f8902896282b6cfa898d53
Daniel <txt2416@autuni.ac.nz>
2021-11-29 16:28:38 +1300

    adjusting background index images and preloaded them

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
+    sameSite: "lax",
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
