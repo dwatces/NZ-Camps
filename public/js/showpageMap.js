ad61ef684e336eae78b9d62532ef32d2b27720f3
Daniel <daniel@danielolliver.tech>
2025-01-06 17:05:09 +1300

    test

--- public/js/showpageMap.js
+++ public/js/showpageMap.js
@@ -1,5 +1,12 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHdhdGNlcyIsImEiOiJjbTVrOGV2YXIwaWx0Mm5wdGluaTN5MHBhIn0.PbSNIOVUgEtqC40Hv4DaTA';
 
+console.log("Raw camp data:", camp);
+console.log("Coordinates field:", camp.geometry.coordinates);
+camp.geometry.coordinates = [172.6812, -34.4329];
+fetch('/api/camps/677b2b8e6396d300158a7ac6')
+  .then(res => res.json())
+  .then(data => console.log("API response:", data))
+
 // Initialize the map
 const map = new mapboxgl.Map({
   container: "map",
