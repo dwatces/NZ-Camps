47c740f59bb578eefd90e5b376dd57f1681e6079
Daniel <daniel@danielolliver.tech>
2025-01-06 19:11:38 +1300

    fixing cluster map

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -1,7 +1,7 @@
 // Initialize the map
 const map = L.map('cluster-map').setView([-41, 174], 4.5);  // Set to New Zealand's latitude and longitude
 
-// Add OpenStreetMap tile layer to the map
+// Set OpenStreetMap tiles (alternative to Mapbox)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
@@ -10,8 +10,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const markers = L.markerClusterGroup();
 
 // Loop through camps and create markers with popup info
-const camps = <%- JSON.stringify(camps) %>; // Ensure camps data is passed from backend
-
 camps.forEach(camp => {
   const marker = L.marker([camp.geometry.coordinates[1], camp.geometry.coordinates[0]])  // [lat, lng]
     .bindPopup(`
@@ -24,3 +22,4 @@ camps.forEach(camp => {
 
 // Add markers to the map with clustering
 map.addLayer(markers);
+
