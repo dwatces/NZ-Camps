c1f51838cccb3805436272c15d4ffe8be3109987
Daniel <daniel@danielolliver.tech>
2025-01-06 14:49:19 +1300

    added mapbox token

--- public/js/showpageMap.js
+++ public/js/showpageMap.js
@@ -1,4 +1,4 @@
-mapboxgl.accessToken = sk.eyJ1IjoiZHdhdGNlcyIsImEiOiJjbTVrOWxyeGMwNXJkMnFwemV2d3B3eGV5In0.XnkmMhRTO1jOJpjTCyfBOQ;
+mapboxgl.accessToken = 'sk.eyJ1IjoiZHdhdGNlcyIsImEiOiJjbTVrOWxyeGMwNXJkMnFwemV2d3B3eGV5In0.XnkmMhRTO1jOJpjTCyfBOQ';
 const map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/outdoors-v11",
